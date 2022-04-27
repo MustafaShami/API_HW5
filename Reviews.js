75a6e7c29f3c13ec10b6e46e79caed5662417c70
MustafaShami <mustafa.shami@ucdenver.edu>
2022-04-27 13:43:35 -0600

    commit from hw4

---
+++ Reviews.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
+var ReviewSchema = new Schema({
+    user: {type: String, required: true},
+    movieTitle: {type: String, required: true},
+    rating: {type: String, required: true},
+    review: {type: String, required: true, index:{unique:true}} //index:true will make mongo database throw code 11000 for duplicated entry
+});
+
+ReviewSchema.pre('save', function(next)
+{
+    var review = this; //maybe?
+    next();
+});
+
+//return the model to server
+module.exports = mongoose.model('Review', ReviewSchema);
